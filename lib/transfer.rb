2aa6007ace91ecbcb90163649b20cc9b6a75aa8f
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-02 00:50:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ def execute_transaction
     receiver.balance += amount
     @status = "complete"
   else
-    "Transaction rejected. Please check your account balance."
     @status = "rejected"
+    "Transaction rejected. Please check your account balance."
 end 
 
 
