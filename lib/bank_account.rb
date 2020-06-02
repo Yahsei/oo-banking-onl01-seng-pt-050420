109a52841a98390bac57b9fe80c403a9b52ed0f5
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-02 00:16:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
-attr_accessor :balance :
+attr_accessor :balance, :status
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @balance = balance
+  @status = pending
+end
+
+
+
+
+
 end
