97c29c065f7d8b2303914f64c34139d46811621e
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-02 00:32:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,13 @@
 class Transfer
+  attr_accessor :sender :receiver :amount :status
   
   def initialize(sender, receiver, amount)
-  
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = pending
+    
+end
   
   
   
